18a3e8813b708a1c549989b3164745c4ecca25d3
Zhijiang Li <zli@brex.com>
2023-10-27 15:39:09 -0700

    patch and install.sh updates

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo chsh $USER -s $(which zsh)
 
 echo "Installing zhjngli env..."
 git clone https://github.com/zhjngli/env.git
-popd env
+cd env
 git apply ../env.patch
 ./env.sh --install git vimrc zsh-theme zshrc
-pushd
+cd -
