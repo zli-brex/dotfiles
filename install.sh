872d91fe35c0ea2d71dbc2525a896d79895d79b0
Zhijiang Li <zli@brex.com>
2023-10-27 15:26:56 -0700

    install.sh updates

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+echo "chsh to zsh..."
+sudo chsh $USER -s $(which zsh)
+
 echo "Installing zhjngli env..."
 git clone https://github.com/zhjngli/env.git
-./env/env.sh --install
+popd env
+./env.sh --install git vimrc zsh-theme zshrc
+pushd
