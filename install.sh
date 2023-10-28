eb6b5491fe35f3b67f75cc5105ce1f329a9d2af5
Zhijiang Li <zli@brex.com>
2023-10-27 17:39:41 -0700

    Add vundle

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
+echo "Installing vundle..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 echo "chsh to zsh..."
 sudo chsh $USER -s $(which zsh)
 
